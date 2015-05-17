54438b5bd276450ff98a3dcdecba8c6264a34509
evgkit <kitaev.ea@gmail.com>
2015-05-17 23:06:12 +0300

    Add mark and print methods for List

--- list.rb
+++ list.rb
@@ -11,13 +11,38 @@ class List
   end
 
   def add_item(name)
-    items.push(Item.new(name))
+    @items.push(Item.new(name))
   end
 
   def remove_item(name)
-    items.each do |item|
-      items.delete(item) if item.name == name
+    @items.each do |item|
+      @items.delete(item) if item.name == name
     end
+  end
+
+  def mark_item(name)
+    @items.each do |item|
+      if item.name == name
+        index = @items.index(item)
+        @items[index].mark_complete!
+      end
+    end
+  end
+
+  def print_list(kind = "all")
+    puts "#{name} List - #{kind} items"
+    puts "-" * 30
+    @items.each do |item|
+      case kind
+        when "all"
+          puts item
+        when "complete"
+          puts item if item.complete?
+        when "incomplete"
+          puts item unless item.complete?
+      end
+    end
+    puts "\n"
   end
 
 end
@@ -27,6 +52,10 @@ list.add_item("Fuse")
 list.add_item("Spice")
 list.add_item("Sword")
 
-puts list.inspect
+
 list.remove_item("Spice")
-puts list.inspect
+list.add_item("Troyan")
+list.mark_item("Sword")
+puts list.print_list
+puts list.print_list("complete")
+puts list.print_list("incomplete")
