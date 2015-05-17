6a1e9b9f40a34da50d6ecb41eb03315d77a16e2c
evgkit <kitaev.ea@gmail.com>
2015-05-17 22:21:07 +0300

    Add remove_item method to List

--- list.rb
+++ list.rb
@@ -10,12 +10,23 @@ class List
     @items = []
   end
 
-  def add_item(item)
-    items.push(Item.new(item))
+  def add_item(name)
+    items.push(Item.new(name))
   end
+
+  def remove_item(name)
+    items.each do |item|
+      items.delete(item) if item.name == name
+    end
+  end
+
 end
 
 list = List.new("Special")
 list.add_item("Fuse")
+list.add_item("Spice")
+list.add_item("Sword")
 
+puts list.inspect
+list.remove_item("Spice")
 puts list.inspect
